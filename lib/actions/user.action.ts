3b8d86401540ae4fe8c1e91aeab26154b0edfd30
Somu878 <somumydhili19@gmail.com>
2024-07-29 15:31:06 +0530

    fix #4 user schema prisma #2

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -10,24 +10,24 @@ export async function createUser(user: any) {
   }
 }
 
-export async function updateUser(user: UpdateUserParams) {
-  try {
-    const updatedUser = await prisma.user.update({
-      where: {
-        email: user.email,
-      },
-      data: {
-        username: user.username!,
-        firstName: user.firstName!,
-        lastName: user.lastName!,
-        photo: user.photo!,
-      },
-    });
-    return JSON.parse(JSON.stringify(updatedUser));
-  } catch (error) {
-    handleError(error);
-  }
-}
+// export async function updateUser(user: UpdateUserParams) {
+//   try {
+//     const updatedUser = await prisma.user.update({
+//       where: {
+//         email: user.email,
+//       },
+//       data: {
+//         username: user.username!,
+//         firstName: user.firstName!,
+//         lastName: user.lastName!,
+//         photo: user.photo!,
+//       },
+//     });
+//     return JSON.parse(JSON.stringify(updatedUser));
+//   } catch (error) {
+//     handleError(error);
+//   }
+// }
 // DELETE
 
 export async function deleteUser(id: any) {
